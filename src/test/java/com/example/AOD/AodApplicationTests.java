9640b3426137b2eb7407d24096c97888378878c4
dfdfg42 <dfdfg11@gmail.com>
2025-04-07 14:18:53 +0900

    first commit

---
+++ src/test/java/com/example/AOD/AodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AOD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
