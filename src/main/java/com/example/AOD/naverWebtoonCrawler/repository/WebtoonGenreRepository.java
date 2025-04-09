9b09c45164a269cd043f68769c80033160e8ba97
강현구 <119887373+ardkyer@users.noreply.github.com>
2025-04-09 16:35:10 +0900

    Revert "[Feat] Implement naver webtoon crawler"

--- src/main/java/com/example/AOD/naverWebtoonCrawler/repository/WebtoonGenreRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.AOD.naverWebtoonCrawler.repository;
-
-import java.util.Optional;
-import com.example.AOD.naverWebtoonCrawler.domain.WebtoonGenre;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WebtoonGenreRepository extends JpaRepository<WebtoonGenre, Long> {
-    Optional<WebtoonGenre> findByGenre(String genre);
-}
