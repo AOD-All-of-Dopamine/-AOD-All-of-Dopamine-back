8fa09364408ad3583326cf3e8f4cfd8e21de7612
unluckyboy214 <janggoon0214@catholic.ac.kr>
2025-04-11 16:53:30 +0900

    [Fix] All movie crawl & genre update

--- src/main/java/com/example/AOD/movie/runner/MovieCrawlerRunner.java
+++ src/main/java/com/example/AOD/movie/runner/MovieCrawlerRunner.java
@@ -18,9 +18,9 @@ public class MovieCrawlerRunner implements CommandLineRunner {
         log.info("영화 크롤링 실행기 시작");
 
         // 최근 영화만 크롤링 (전체 크롤링은 주석을 해제하여 사용)
-        // cgvCrawler.crawlAll(); // 주의: 첫 실행 시에만 사용 (모든 영화를 크롤링)
+        cgvCrawler.crawlAll(); // 주의: 첫 실행 시에만 사용 (모든 영화를 크롤링)
 
-        cgvCrawler.crawlRecent(); // 최신 영화만 크롤링
+        //cgvCrawler.crawlRecent(); // 최신 영화만 크롤링
 
         log.info("영화 크롤링 실행기 완료");
     }
