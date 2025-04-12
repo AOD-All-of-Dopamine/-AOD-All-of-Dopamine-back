d101b341ab1dc4c4bf9087c7346b9ca8c277eff4
고경우 <dfdfg11@gmail.com>
2025-04-12 11:55:23 +0900

    [Feat] 다대다 설정을 위한 넷플릭스 도메인설정

--- src/main/java/com/example/AOD/OTT/Netflix/domain/NetflixContent.java
+++ src/main/java/com/example/AOD/OTT/Netflix/domain/NetflixContent.java
@@ -1,31 +1,77 @@
 package com.example.AOD.OTT.Netflix.domain;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
 @Entity
+@Table(name = "netflix_content")
 public class NetflixContent {
 
     @Id
-    @Column(name = "content_id",nullable = false ,length = 255)
+    @Column(name = "content_id", length = 255, nullable = false)
     private String contentId;
 
-    @Column(nullable = false,length = 255)
+    @Column(nullable = false, length = 255)
     private String title;
 
-    @Column(name = "synopsis", columnDefinition = "TEXT")
-    private String synopsis;
+    @Column(length = 50)
+    private String type;
 
-    @Column(name = "thumbnail_url", nullable = false, columnDefinition = "TEXT")
-    private String thumbnailUrl;
+    @Column(name = "main_url", length = 500)
+    private String url;
+
+    @Column(name = "detail_url", length = 500)
+    private String detailUrl;
+
+    @Column(columnDefinition = "TEXT")
+    private String thumbnail;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(length = 500)
+    private String creator;
 
     @Column(name = "maturity_rating", length = 20)
     private String maturityRating;
 
     @Column(name = "release_year", length = 4)
     private String releaseYear;
+
+    private LocalDateTime crawledAt;
+
+    // 배우 리스트 (ManyToMany)
+    @ManyToMany
+    @JoinTable(
+            name = "netflix_content_actor",
+            joinColumns = @JoinColumn(name = "content_id"),
+            inverseJoinColumns = @JoinColumn(name = "actor_id")
+    )
+    private List<Actor> actors;
+
+    // 장르 리스트 (ManyToMany)
+    @ManyToMany
+    @JoinTable(
+            name = "netflix_content_genre",
+            joinColumns = @JoinColumn(name = "content_id"),
+            inverseJoinColumns = @JoinColumn(name = "genre_id")
+    )
+    private List<Genre> genres;
+
+    // 특징 리스트 (ManyToMany)
+    @ManyToMany
+    @JoinTable(
+            name = "netflix_content_feature",
+            joinColumns = @JoinColumn(name = "content_id"),
+            inverseJoinColumns = @JoinColumn(name = "feature_id")
+    )
+    private List<Feature> features;
 }
