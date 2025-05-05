e11030fde415eddc8695f0a80065a8618d557e2e
5ms <oms010414@gmail.com>
2025-05-05 17:26:19 +0900

    [Fix] Fix SteamGame's properties

--- src/main/java/com/example/AOD/game/StreamAPI/repository/GameRepository.java
+++ src/main/java/com/example/AOD/game/StreamAPI/repository/GameRepository.java
@@ -1,7 +1,7 @@
 package com.example.AOD.game.StreamAPI.repository;
 
-import com.example.AOD.game.StreamAPI.domain.Game;
+import com.example.AOD.game.StreamAPI.domain.SteamGame;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GameRepository extends JpaRepository<Game,Long> {
+public interface GameRepository extends JpaRepository<SteamGame,Long> {
 }
