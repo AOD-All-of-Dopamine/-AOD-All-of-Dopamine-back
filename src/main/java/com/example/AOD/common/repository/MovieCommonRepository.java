fc19d1ac617af674d479ea9acebac9392cf6740b
고경우 <dfdfg11@gmail.com>
2025-05-09 23:08:10 +0900

    [Feat] common 설정, 통합로직 스켈레톤 코드 -> 설정,통합을 모든 미디어 컨텐츠에 범용 로직을 사용하도록 변경

---
+++ src/main/java/com/example/AOD/common/repository/MovieCommonRepository.java
@@ -0,0 +1,8 @@
+package com.example.AOD.common.repository;
+
+import com.example.AOD.common.commonDomain.MovieCommon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MovieCommonRepository extends JpaRepository<MovieCommon, Long> {
+
+}
