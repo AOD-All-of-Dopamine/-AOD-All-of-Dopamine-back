385d9ddf4e26a096d0b016aa4909cf30c5660504
unluckyboy214 <janggoon0214@catholic.ac.kr>
2025-05-11 19:41:39 +0900

    [Feat] Avoid duplicate select - title

--- src/main/java/com/example/AOD/common/repository/GameCommonRepository.java
+++ src/main/java/com/example/AOD/common/repository/GameCommonRepository.java
@@ -3,5 +3,9 @@ package com.example.AOD.common.repository;
 import com.example.AOD.common.commonDomain.GameCommon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface GameCommonRepository extends JpaRepository<GameCommon, Long> {
+    List<GameCommon> findByTitleIgnoreCase(String title);
+    List<GameCommon> findByTitleContainingIgnoreCase(String keyword);
 }
--- src/main/java/com/example/AOD/common/repository/OTTCommonRepository.java
+++ src/main/java/com/example/AOD/common/repository/OTTCommonRepository.java
@@ -3,5 +3,9 @@ package com.example.AOD.common.repository;
 import com.example.AOD.common.commonDomain.OTTCommon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OTTCommonRepository extends JpaRepository<OTTCommon, Long> {
+    List<OTTCommon> findByTitleIgnoreCase(String title);
+    List<OTTCommon> findByTitleContainingIgnoreCase(String keyword);
 }
