261b67155e380f697666b8b16c2e927d9b3f4cb6
5ms <oms010414@gmail.com>
2025-05-11 20:14:55 +0900

    [Fix] Fix SteamApiFetcherTest and API performance Test

--- src/test/java/com/example/AOD/StreamAPI/SteamApiFetcherTest.java
+++ src/test/java/com/example/AOD/StreamAPI/SteamApiFetcherTest.java
@@ -6,17 +6,39 @@ import com.example.AOD.game.StreamAPI.dto.GameDetailDto.GameDetailDto;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 class SteamApiFetcherTest {
 
     private final SteamApiFetcher steamApiFetcher = new SteamApiFetcher();
     @Test
-    public void test(){
-        List<SimpleGameDto> allGameId = steamApiFetcher.getAllGame();
-        for(int i=0;i<100;i++){
+    public void test() throws InterruptedException {
+        /*
+        매 200번째 요청마다 429 Too Many Requests 발생 : 5분 동안 요청 불가
+        게임 1000개 가져오는데 약 23분
+        전체 게임 다운로드하는 데 약 95시간
+         */
+
+        List<SimpleGameDto> allGameId = steamApiFetcher.getAllGame(); //size = 246'181
+        long st = System.currentTimeMillis();
+
+        for(int i=0;i<1000;i++){
             SimpleGameDto game = allGameId.get(i);
-            System.out.println(game.getAppid()+" "+game.getName());
+            try {
+                GameDetailDto g = steamApiFetcher.getGameDetailById(game.getAppid(), "korean");
+                if(g!=null){
+                    System.out.println(i+": "+g.getSteam_appid()+" "+g.getName());
+                }
+            } catch (Exception e){
+                System.out.println(e.getMessage());
+                Thread.sleep(60*1000);
+                i--;
+            }
         }
+
+        long en = System.currentTimeMillis();
+        System.out.println("실행 시간: " + (en - st) + "ms");
+
     }
 
     @Test
