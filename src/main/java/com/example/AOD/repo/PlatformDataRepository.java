c52235746360c51fdb5a647ae9c273f39d3796f3
unluckyboy214 <janggoon0214@catholic.ac.kr>
2025-09-24 19:44:10 +0900

    Feat: 작품별 상세페이지 추가

--- src/main/java/com/example/AOD/repo/PlatformDataRepository.java
+++ src/main/java/com/example/AOD/repo/PlatformDataRepository.java
@@ -1,10 +1,13 @@
 package com.example.AOD.repo;
 
+import com.example.AOD.domain.Content;
 import com.example.AOD.domain.entity.PlatformData;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PlatformDataRepository extends JpaRepository<PlatformData, Long> {
     Optional<PlatformData> findByPlatformNameAndPlatformSpecificId(String platformName, String platformSpecificId);
+    List<PlatformData> findByContent(Content content);
 }
